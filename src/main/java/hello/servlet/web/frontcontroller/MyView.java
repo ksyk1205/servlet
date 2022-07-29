49e8e40032a1abb3a077db5c6969c7caaa292eed
Rosy(곽세연) <sygwak@gabia.com>
2022-07-29 17:54:55 +0900

    Model추가 - v3 (오탈자 수정)

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -20,12 +20,12 @@ public class MyView {
     }
 
     public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        modeltToRequestAttribute(model, request);
+        modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
 
-    private void modeltToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
         model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
